fb49f3201aa5af1188192e68a003168caec992b4
Debaditya Mukhopadhyay <debaditya.mukhopadhyay@aaqua.live>
2021-11-13 21:45:13 +0530

    relations

--- Extract_Relations.py
+++ Extract_Relations.py
@@ -1,10 +1,8 @@
-# deploy ; Movement:Transport ; 17,000 U.S. Army soldiers ; PER:Group ; Artifact | deploy ; Movement:Transport ; the Persian Gulf region ; LOC:Region-International ; Destination
-
 Event_types = set()
 Arguments = set()
 Roles = set()
 
-with open("all_tuples.txt", 'r') as f:
+with open("event_data_word/all_tuples.txt", 'r') as f:
     file = f.readlines()
 
     for line in file:
@@ -15,17 +13,23 @@ with open("all_tuples.txt", 'r') as f:
             Arguments.add(phrases[3].strip())
             Roles.add(phrases[4].strip())
 
-with open("event_types.txt", 'w') as f:
+with open("event_data_word/event_types.txt", 'w') as f:
+    Event_types = list(Event_types)
+    Event_types.sort()
     for L in Event_types:
         f.write(L)
         f.write("\n")
 
-with open("arguments.txt", 'w') as f:
+with open("event_data_word/arguments.txt", 'w') as f:
+    Arguments = list(Arguments)
+    Arguments.sort()
     for L in Arguments:
         f.write(L)
         f.write("\n")
 
-with open("roles.txt", 'w') as f:
+with open("event_data_word/roles.txt", 'w') as f:
+    Roles = list(Roles)
+    Roles.sort()
     for L in Roles:
         f.write(L)
         f.write("\n")
